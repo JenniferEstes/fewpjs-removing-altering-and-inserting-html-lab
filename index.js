521377030dded5e065a0dd7cfc1b4c0a219eeb4b
Jennifer <jenndestes@gmail.com>
2021-05-12 21:08:52 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Write your code here!
+let removeDom = document.querySelector('#main').remove();
+
+let newHeader = document.createElement('h1');
+document.body.appendChild(newHeader);
+newHeader.id = 'victory';
+document.querySelector('h1#victory');
+newHeader.innerHTML = 'Jenny is the champion';
